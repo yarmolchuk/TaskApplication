245124034d3d6e6338ee67169fa5b3dbc939361c
yarmolchuk <yarmolchuk@gmail.com>
2015-01-28 23:06:54 +0200

    Initial Commit

---
+++ TaskApplication/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TaskApplication
+//
+//  Created by Dima on 28.01.15.
+//  Copyright (c) 2015 Dima. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
